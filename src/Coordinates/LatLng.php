8532bee96ee6c51080f046d69eed616e0d177a7e
Cees-Jan <ceesjank@gmail.com>
2014-08-17 13:57:32 +0200

    Ported over from StaticMap

--- src/Coordinates/LatLng.php
+++ src/Coordinates/LatLng.php
@@ -3,98 +3,107 @@
 /*
  * This file is part of HotSpots.
  *
- * (c) 2012 - 2013 Cees-Jan Kiewiet
+ * (c) 2012 Cees-Jan Kiewiet
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-namespace WyriHaximus\HotSpots\Coordinates;
+namespace WyriHaximus\StaticMap;
 
 /**
  * Storage for lat, lng pair values.
  *
- * @package HotSpots
+ * @package StaticMap
  * @author  Cees-Jan Kiewiet <ceesjank@gmail.com>
  */
-final class LatLng {
-    
+final class LatLng
+{
     /**
      * Value for the lat
-     * @var int
+     * @var double
      */
     private $lat;
-    
+
     /**
      * Value for the lng
-     * @var int
+     * @var double
      */
     private $lng;
-    
+
     /**
      * Store Size value's.
-     * 
+     *
      * All value's will be sanitized and forced in a certain range.
-     * 
-     * @param int $lat lat
-     * @param int $lng lng
+     *
+     * @param double $lat lat
+     * @param double $lng lng
      */
-    public function __construct($lat, $lng) {
+    public function __construct($lat, $lng)
+    {
         $this->setLat($lat);
         $this->setLng($lng);
     }
-    
+
     /**
      * Set the value for the lat and returns the stored value
-     * @param type $lat 
-     * @return int 
+     * @param  double $lat
+     * @return double
      */
-    public function setLat($lat) {
-        $this->lat = $this->sanitized($lat, -90, 90);
+    public function setLat($lat)
+    {
+        $this->lat = $this->sanitized($lat, -90, 90, $this->lat);
+
         return $this->lat;
     }
-    
+
     /**
      * Returns the value for the lat
-     * @return int 
+     * @return double
      */
-    public function getLat() {
+    public function getLat()
+    {
         return $this->lat;
     }
-    
+
     /**
      * Set the value for the lng and returns the stored value
-     * @param type $lng 
-     * @return int 
+     * @param  double $lng
+     * @return double
      */
-    public function setLng($lng) {
-        $this->lng = $this->sanitized($lng, -180, 180);
+    public function setLng($lng)
+    {
+        $this->lng = $this->sanitized($lng, -180, 180, $this->lng);
+
         return $this->lng;
     }
-    
+
     /**
      * Returns the value for the lng
-     * @return int 
+     * @return double
      */
-    public function getLng() {
+    public function getLng()
+    {
         return $this->lng;
     }
-    
+
     /**
      * Sanitize value to be int and between $rangeBegin and $rangeend.
-     * 
-     * @param int $value Value to be sanitized
-     * @param int $rangeBegin Begin of the range
-     * @param int $rangeEnd End of the range
-     * @return int Sanitized and correctly forced value 
+     *
+     * @param  double $value      Value to be sanitized
+     * @param  double $rangeBegin Begin of the range
+     * @param  double $rangeEnd   End of the range
+     * @param  double $default    Value to use incase $value is invalid
+     * @return double
      */
-    private function sanitized($int, $rangeBegin, $rangeEnd) {
-        $int = floatval($int);
-        
-        if ($int >= $rangeBegin && $rangeEnd >= $int) {
-            return $int;
+    private function sanitized($value, $rangeBegin, $rangeEnd, $default)
+    {
+        $value = floatval($value);
+
+        if ($value >= $rangeBegin && $rangeEnd >= $value) {
+            return $value;
         } else {
-            return false;
+            return $default;
         }
     }
 
